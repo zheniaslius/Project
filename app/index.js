411e79d98bf0d7af2490572fde6acd0451bb454a
ZheniaFIT <zhen0077@ukr.net>
2018-11-06 11:37:16 +0200

    20%

---
+++ app/index.js
@@ -0,0 +1,11 @@
+/**
+ * Application entry point
+ */
+
+// Load application styles
+import 'reset-css';
+import 'styles/index.scss';
+
+// ================================
+// START YOUR APP HERE
+// ================================

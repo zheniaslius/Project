262c5bdf13a284dab5d434a659ccbd351f181ba5
ZheniaFIT <zhen0077@ukr.net>
2018-11-21 21:07:09 +0200

    second page

--- assets/services/services.js
+++ assets/services/services.js
@@ -5,6 +5,7 @@ import './styles.scss'
 import Glide from '@glidejs/glide'
 
 const glide = new Glide('.glide', {
+    gap: 0,
     perView: 3,
     focusAt: 'center',
     startAt: 1,
@@ -12,34 +13,10 @@ const glide = new Glide('.glide', {
 
 const services = [...document.querySelectorAll('.feature-desc')]
 
-const getNext = i => {
+const toShow = i => {
     services.map((element, pos) => {
         element.style.display = (pos == i) ? 'block' : 'none'
     })
-    // const toShow = services[i] 
-    // toShow.style.display = 'block'
 }
 
-glide.on('move', () => getNext(glide.index))
-
-const next = document.querySelector('.arrow-next')
-const prev = document.querySelector('.arrow-prev')
-
-
-// const getPrevious = i => {
-//     const prevItem = document.querySelector(`body > section:nth-of-type(${(i == 6) ? 1 : i+1})`)
-//     prevItem.style.display = 'none'
-//     const toShow = document.querySelector(`body > section:nth-of-type(${i})`)
-//     toShow.style.display = 'block'
-// }
-
-// let i = 1
-// next.addEventListener('click', () => {
-//     i = (i < 6) ? ++i : 1
-//     getNext(i)
-// })
-
-// prev.addEventListener('click', () => {
-//     i = (i > 1) ? --i : 6
-//     getPrevious(i)
-// })
+glide.on('move', () => toShow(glide.index))

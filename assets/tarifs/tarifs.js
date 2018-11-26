05b8ea86c494fbc365019e06baa70d190c58c888
ZheniaFIT <zhen0077@ukr.net>
2018-11-26 22:26:32 +0200

    mobile 3rd

--- assets/tarifs/tarifs.js
+++ assets/tarifs/tarifs.js
@@ -2,5 +2,26 @@ import 'reset-css'
 import '../styles/index.scss'
 import '../services/styles.scss'
 import './styles.scss'
+import './media.scss'
 
-import '../js/hamburger.js';
+import '../js/hamburger.js'
+
+const nav = document.querySelector('.nav__wrp')
+const tables = [...document.querySelectorAll('.tarifs__table')]
+
+const handleMenu = (e) => {
+    const selected = e.target;
+    const tabs = [...document.querySelectorAll('.nav__tab')]
+    tabs.forEach((tab, i) => {
+        if (tab == selected) {
+            selected.classList.add('tab--active')
+            tables[i].style.display = 'flex'
+        }
+        else {
+            tab.classList.remove('tab--active')
+            tables[i].style.display = 'none'
+        }
+    })
+}
+
+nav.addEventListener('click', handleMenu)
